8075879005c93e74fe76a6c10f1b01e8c5cd94c9
Gabriele Sipione <49779310+GabrieleSipio@users.noreply.github.com>
2023-07-29 22:24:53 +0000

    restructoring

--- Part1/main.py
+++ Part1/main.py
@@ -1,25 +1,71 @@
 '''Python course part 1'''
 import json
+import random
+
+
+def NegativeToPositive(x):
+    '''check if an element is negative and makes it positive'''
+    return x * -1 if x < 0 else x
+
+
+def TakeEven(inputDict):
+    '''takes the even numbers of a dict'''
+    return {key: [idx for idx in val if not idx % 2]  
+          for key, val in inputDict.items()}  
+
+
+def TakeOdd(inputDict):
+    '''takes the odd numbers of a dict'''
+    return {key: (lambda val: val % 2) for key, val in inputDict.items()}
+
+
+def CreateRandomDict(dictLenght):
+    '''create a dict with random values'''
+    myDict = {}
+    for i in range(dictLenght):
+        myDict.update({f"key{i}": random.randint(-100, 100)})
+    return myDict
+
 
 helloWorld = "Hello World!"
 print(helloWorld)
-x = 1
-print(x)
-print(f"{helloWorld} this is string interpolation attempt {x}")
-aDict = {"a": 54, "b": 87, "c": 56, "d": 875}
+tries = 1
+print(tries)
+print(f"{helloWorld} this is string interpolation attempt {tries}")
+dictLenght = int(input("instert dict lenght: "))
+aDict = CreateRandomDict(dictLenght)
+evenDict = TakeEven(aDict)
+oddDict = TakeOdd(aDict)
+path = "Files/data.json"
+pathEven = "Files/dataEven.json"
+pathOdd = "Files/dataOdd.json"
 jsonString = json.dumps(aDict)
-with open("data.json", "w+") as jsonFile:
+with open(path, "w+") as jsonFile:
     jsonFile.write(jsonString)
-with open("data.json", "r+") as jsonFile:
+with open(pathEven, "w+") as jsonFile:
+    print(f"even values in dict:\n{evenDict}")
+    jsonFile.write(json.dumps(evenDict, indent=4))
+with open(pathOdd, "w+") as jsonFile:
+    print(f"odd values in dict:\n{oddDict}")
+    jsonFile.write(json.dumps(oddDict, indent=4))
+with open(path, "r+") as jsonFile:
     jsonContent = jsonFile.read()
     aDict = json.loads(jsonContent)
     print(f"current dict {aDict}")
-newValue = int(input("insert new value: "))
-with open("data.json", "w+") as jsonFile:
-    aDict.update({"e": newValue})
-    print(f"updated dict {aDict}")
     checkPositives = all(value > 0 for value in (aDict.values()))
     message = "All positives!" if checkPositives else "Not all positives!"
+    print(f"updated dict {aDict}")
     print(message)
-    newJsonString = json.dumps(aDict, indent=4)
-    jsonFile.write(newJsonString)
+    if not checkPositives:
+        aDict = {k: NegativeToPositive(v) for k, v in aDict.items()}
+        evenDict = {k: NegativeToPositive(v) for k, v in evenDict.items()}
+        oddDict = {k: NegativeToPositive(v) for k, v in oddDict.items()}
+        print(f"balanced dictionary:\n{aDict}")
+        print(f"balanced even dictionary:\n{evenDict}")
+        print(f"balanced odd dictionary:\n{oddDict}")
+with open(path, "w+") as jsonFile:
+    jsonFile.write(json.dumps(aDict, indent=4))
+with open(pathEven, "w+") as jsonFile:
+    jsonFile.write(json.dumps(evenDict, indent=4))
+with open(pathOdd, "w+") as jsonFile:
+    jsonFile.write(json.dumps(oddDict, indent=4))
