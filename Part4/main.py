89efedee609738c5e505efdfc76a95f05bf666fd
Gabriele Sipione <49779310+GabrieleSipio@users.noreply.github.com>
2024-02-04 17:20:42 +0100

    part 4

---
+++ Part4/main.py
@@ -0,0 +1,9 @@
+'''working with data structures and classes but with exception handling'''
+import sys
+sys.path.append('/home/jager/Documenti/GitHub/pythonCourse')
+
+
+
+
+if __name__ == '__main__':
+    print("starting")
