c35a3a90352dfd9b516d8bb3a22991591bab08c6
Gabriele Sipione <49779310+GabrieleSipio@users.noreply.github.com>
2024-03-26 20:44:08 +0100

    first part web scraper

--- Part4/main.py
+++ Part4/main.py
@@ -1,11 +1,21 @@
 '''working with API'''
 import sys
+import requests
+from bs4 import BeautifulSoup
 # sys.path.append('/home/jager/Documenti/GitHub/pythonCourse')
 sys.path.append('..\\pythonCourse') #when on Windows
 
-
-
+def WebScraper():
+    response = requests.get('https://stackoverflow.com/questions', timeout = 2.50)
+    soup = BeautifulSoup(response.text, "html.parser")
+    questions = soup.select(".s-post-summary")
+    questionText = [{question.select_one('.s-link').getText(),question.select_one('.s-post-summary--stats-item-number').getText()} for question in questions]
+    print(type(questions[0]))
+    print(f"question count : {len(questionText)}")
+    for item in questionText:
+        print(item)
 
 
 if __name__ == '__main__':
+    WebScraper()
     print("starting")
